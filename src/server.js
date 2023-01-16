d91e74739dd633fc42ea0139fb85f88b8b7cfd86
jocgsousa <gregoriociacom@gmail.com>
2023-01-16 19:35:29 -0300

    App updated

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express, { Router } from "express";
 import fs from "fs";
 import path from "path";
-// import { createServer } from "https";
-import { createServer } from "http";
+import { createServer } from "https";
+// import { createServer } from "http";
 import cors from "cors";
 import "dotenv/config";
 
@@ -11,9 +11,29 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(__dirname, { dotfiles: "allow" }));
 
-const config = {
-  key: fs.readFileSync(path.join(__dirname, "certs", "key.pem")),
-  cert: fs.readFileSync(path.join(__dirname, "certs", "cert.pem")),
+// const config = {
+//   key: fs.readFileSync(path.join(__dirname, "certs", "key.pem")),
+//   cert: fs.readFileSync(path.join(__dirname, "certs", "cert.pem")),
+// };
+
+// Certificate
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/schhsoinbit.ga/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/schhsoinbit.ga/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync(
+  "/etc/letsencrypt/live/schhsoinbit.ga/fullchain.pem",
+  "utf8"
+);
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
 };
 
 const routes = new Router();
@@ -24,7 +44,7 @@ routes.get("/", (req, res) => {
 
 app.use(routes);
 
-const server = createServer(config, app);
+const server = createServer(credentials, app);
 
 server.listen(process.env.PORT || 8080, () => {
   console.log(`Server listen in port: ${process.env.PORT || 8080}`);
