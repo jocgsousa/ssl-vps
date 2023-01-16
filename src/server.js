0b7f76997f315c944433466e9acbfccd99b56a78
jocgsousa <gregoriociacom@gmail.com>
2023-01-16 19:13:56 -0300

    App updated

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import "dotenv/config";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(__dirname, { dotfiles: "allow" }));
 
 const config = {
   key: fs.readFileSync(path.join(__dirname, "certs", "key.pem")),
