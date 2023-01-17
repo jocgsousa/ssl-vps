67a29fa1b74cba5313f68c243af43da2b9b0b5cf
jocgsousa <gregoriociacom@gmail.com>
2023-01-16 22:02:32 -0300

    App updated

--- src/server.js
+++ src/server.js
@@ -12,6 +12,14 @@ app.use(cors());
 app.use(express.static(__dirname, { dotfiles: "allow" }));
 
 // FORCE HTTPS REQUESTS
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
 
 // Certificate
 const privateKey = fs.readFileSync(
