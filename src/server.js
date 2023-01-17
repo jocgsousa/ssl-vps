bfe99dcd6d599d4e3c8e4f203a4af963b612fd83
jocgsousa <gregoriociacom@gmail.com>
2023-01-16 21:42:13 -0300

    App updated

--- src/server.js
+++ src/server.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(__dirname, { dotfiles: "allow" }));
+// FORCE HTTPS REQUESTS
+app.use(function (request, response, next) {
+  if (process.env.NODE_ENV !== "development" && !request.secure) {
+    return response.redirect("https://" + request.headers.host + request.url);
+  }
+  next();
+});
 
 // Certificate
 const privateKey = fs.readFileSync(
